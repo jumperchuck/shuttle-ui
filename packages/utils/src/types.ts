58d67329368610f3d23159883fad7a9ba4146a94
jumperchuck <ck_125s@163.com>
2021-12-13 02:42:25 +0800

    feat(utils): add RenderProps type

--- packages/utils/src/types.ts
+++ packages/utils/src/types.ts
@@ -1,1 +1,10 @@
+import React from 'react';
+
 export type RecursivePartial<T> = { [P in keyof T]?: RecursivePartial<T[P]> };
+
+export type RenderProps<T> =
+  | string
+  | number
+  | T
+  | React.ReactNode
+  | ((props: Partial<T>) => React.ReactNode);
